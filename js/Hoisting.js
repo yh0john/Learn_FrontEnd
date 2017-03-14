5fc1297e1219be7e0c948266bd253a48ae5b3689
Yohan Lee <yh0john@gmail.com>
2017-03-15 00:34:10 +0900

    호이스팅 ㅔ스트

---
+++ js/Hoisting.js
@@ -0,0 +1,18 @@
+
+console.log(add(2,3))
+
+//함수 선언문 형태로 add() 함수 정의
+
+function add(x,y){
+    return x+y;
+};
+
+console.log(add(3,4))
+
+console.log(plus(3,4));
+
+var plus = function(x,y){
+    return x+y;
+};
+
+console.log(5,6);
