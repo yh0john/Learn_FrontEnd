78f72b4b76b2a628b5973b65b3c1615d7e60c668
Yohan Lee <yh0john@gmail.com>
2017-03-24 00:38:53 +0900

    Excution Context 실습

---
+++ js/ExcutionContext.js
@@ -0,0 +1,16 @@
+/**
+ * Created by John on 2017-03-24.
+ */
+
+console.log("This is global context");
+
+function ExContext1(){
+    console.log("This is ExContext1");
+};
+
+function ExContext2(){
+    ExContext1();
+    console.log("This is ExContext2");
+};
+
+ExContext2();
