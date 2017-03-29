e9ed0d3f473c90c3ae973cffa912984408b9389d
Yohan Lee <yh0john@gmail.com>
2017-03-29 22:12:32 +0900

    form element controll

--- js/protoType.js
+++ js/protoType.js
@@ -3,5 +3,16 @@
  */
 
 var createFormObject ={
-    
+    createFormElement:function(name,method,target,action){
+        var form  = document.createElement('form');
+        form.name = name;
+        form.method = method;
+        form.target = target;
+        form.action = action;
+    },
+    addHiddenValue:function(formName,value){
+        var hiddenInput = document.createElement("input");
+        hiddenInput.type = 'hidden';
+        hiddenInput.value = value;
+        formName.appendChild(hiddenInput);
 };
