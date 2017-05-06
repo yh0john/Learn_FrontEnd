badf6fdb001cce3239effec130cf6e0c580f6b83
Yohan Lee <yh0john@gmail.com>
2017-05-06 22:30:02 +0900

    커링 실습

--- js/Functional Programming in JS/FP Example.js
+++ js/Functional Programming in JS/FP Example.js
@@ -1,1 +1,21 @@
-/*JS init*/
+/*커링 - 특정 함수에서 정의된 인자의 일부를 넣어 고정 시키고 나머지를 인자로 받는 새로운 함수를 만드는 것*/
+
+//예제 7.7 커링 연습하기
+
+//커링 대상 function
+function calculate(a,b,c){
+    return a*b+c;
+}
+
+//커링 function 작성
+function curry(func){
+    var args = Array.prototype.slice.call(arguments,1);
+
+    return function() {
+        return func.apply(null, args.concat(Array.prototype.slice.call(arguments)));
+    }
+}
+
+//커링 예제 테스트 코드
+var new_func1 = curry(calculate,1);
+console.log(new_func1(2,3));
