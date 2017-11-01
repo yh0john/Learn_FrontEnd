f6719319946a3108f0fe1ebd894d0296e7666df1
Yohan Lee <yh0john@gmail.com>
2017-11-01 23:15:36 +0900

    Momoization pattern 실습

--- js/ch07/7-3-3.js
+++ js/ch07/7-3-3.js
@@ -3,10 +3,27 @@
 *
 * memoization pattern
 *
+* Closure,Javascript Object,Memoization pattern
 *
 * */
 
 var fact = function () {
     var cache = {'0':1};
-    
-}
+
+    var func = function (n) {
+        var result = 0;
+
+        if(typeof(cache[n])==='number'){
+            return cache[n];
+        }else{
+            result = cache[n] = n*fact(n-1);
+        }
+
+        return result;
+    }
+
+    return func;
+}();
+
+console.log(fact(10));
+console.log(fact(5));
